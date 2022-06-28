5b9559157b6f615842cd848810bcdaa7eabb5641
timmaier98 <54843889+timmaier98@users.noreply.github.com>
2022-06-28 22:11:06 +0200

    added files

---
+++ Assets/endScreenScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class endScreenScript : MonoBehaviour
+{
+
+     public ScoreScript scoreS;
+     public Text TextFieldScore;
+
+     public xPosScript xPosS;
+     public Text TextFieldXpos;
+
+     public float levelLength = 24641.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextFieldScore.text = "Score: " + ((int)(scoreS.score/10)).ToString();
+        TextFieldXpos.text = "Level was " + ((int)(((xPosS.xValue)/levelLength)*100)).ToString() + " % completed!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
